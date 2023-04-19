8237ec4f509605771004cf54504d432daf0e60b1
SstealzZ <antoine.veber@epitech.eu>
2023-04-19 05:45:33 +0200

    Modify App.java

--- src/main/java/fr/sstealzz/App.java
+++ src/main/java/fr/sstealzz/App.java
@@ -1,13 +1,17 @@
 package fr.sstealzz;
 
+import java.io.IOException;
+
+import fr.sstealzz.utilities.Compressor;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException {
+        Compressor compressor = new Compressor();
+        compressor.compress_mkv("test");
     }
 }
