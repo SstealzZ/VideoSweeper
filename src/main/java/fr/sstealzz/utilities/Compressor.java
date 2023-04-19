d433810cb0de046557fa5aca70d5d36676591519
SstealzZ <antoine.veber@epitech.eu>
2023-04-19 05:46:34 +0200

    Modify Compressor.java - Compressor is now functional

--- src/main/java/fr/sstealzz/utilities/Compressor.java
+++ src/main/java/fr/sstealzz/utilities/Compressor.java
@@ -1,7 +1,34 @@
 package fr.sstealzz.utilities;
 
+import java.io.File;
+import java.io.IOException;
+
+import net.bramp.ffmpeg.FFmpeg;
+import net.bramp.ffmpeg.FFmpegExecutor;
+import net.bramp.ffmpeg.FFprobe;
+import net.bramp.ffmpeg.builder.FFmpegBuilder;
+
 public class Compressor {
-    public void compress_mkv(String path) {
-        //Add compression for mkv
+
+    public void compress_mkv(String path) throws IOException {
+        FFmpeg ffmpeg = new FFmpeg("/bin/ffmpeg");
+        FFprobe ffprobe = new FFprobe("/bin/ffprobe");
+        
+        File file = new File(path + ".mkv");
+        File output = new File(path + "compressed" + ".mkv");
+
+        FFmpegBuilder builder = new FFmpegBuilder()
+            .setInput(file.getAbsolutePath())
+            .overrideOutputFiles(true)
+            .addOutput(output.getAbsolutePath())
+            .setVideoCodec("libx264")
+            .setVideoFrameRate(30, 1)
+            .setVideoResolution(1920, 1080)
+            .setStrict(FFmpegBuilder.Strict.EXPERIMENTAL)
+            .done();
+
+        FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
+        executor.createJob(builder).run();
+        System.out.println("Compressed " + file.getName());
     }
 }
