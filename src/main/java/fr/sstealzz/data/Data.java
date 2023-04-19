b20c7587a500ad700675bb961d7e3bc5d62a25e3
SstealzZ <antoine.veber@epitech.eu>
2023-04-19 09:34:56 +0200

    Modify Data.java - Optimise Data class

--- src/main/java/fr/sstealzz/data/Data.java
+++ src/main/java/fr/sstealzz/data/Data.java
@@ -1,22 +1,18 @@
 package fr.sstealzz.data;
 
 public class Data {
-    public enum DataType {
+    public enum Types {
         NameFile, SizeFile, CompressedSizeFile, CompressionRatio, isCompressed
     }
 
     private String nameFile;
     private long sizeFile;
     private long compressedSizeFile;
-    private double compressionRatio;
-    private boolean isCompressed;
 
     public Data(String nameFile, long sizeFile, long compressedSizeFile, double compressionRatio, boolean isCompressed) {
         this.nameFile = nameFile;
         this.sizeFile = sizeFile;
         this.compressedSizeFile = compressedSizeFile;
-        this.compressionRatio = compressionRatio;
-        this.isCompressed = isCompressed;
     }
 
     public String getNameFile() {
@@ -44,18 +40,11 @@ public class Data {
     }
 
     public double getCompressionRatio() {
-        return compressionRatio;
-    }
-    
-    public void setCompressionRatio(double compressionRatio) {
-        this.compressionRatio = compressionRatio;
+        return (double) compressedSizeFile / sizeFile;
     }
 
     public boolean isCompressed() {
-        return isCompressed;
+        return compressedSizeFile < sizeFile;
     }
 
-    public void setCompressed(boolean isCompressed) {
-        this.isCompressed = isCompressed;
-    }
 }
