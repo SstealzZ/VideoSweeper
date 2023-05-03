781f59b337ea5ea26fec0ea2bae56e32db6cabb7
SstealzZ <antoine.veber@epitech.eu>
2023-05-03 19:21:57 +0200

    update main.java

--- src/main/java/fr/sstealzz/App.java
+++ src/main/java/fr/sstealzz/App.java
@@ -1,8 +1,14 @@
 package fr.sstealzz;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
+import fr.sstealzz.data.Data;
+import fr.sstealzz.data.FileExplorer;
+import fr.sstealzz.data.Json;
 import fr.sstealzz.utilities.Compressor;
+import fr.sstealzz.utilities.Init;
 
 /**
  * Hello world!
@@ -11,7 +17,16 @@ import fr.sstealzz.utilities.Compressor;
 public class App 
 {
     public static void main( String[] args ) throws IOException {
+        Json json = new Json("config.json");
+        json.init();
+        FileExplorer fileExplorer = new FileExplorer();
+        List<File> files = fileExplorer.findVideoFiles();
+        List<Data> datas = json.getData();
         Compressor compressor = new Compressor();
-        compressor.compress_mkv("test");
+        Init init = new Init();
+        init.Init(files, datas, fileExplorer);
+        // Test test = new Test();
+        // test.testJson("config.json");
+        // compressor.compress_mkv("test");
     }
 }
