b4257743902b3e02eb154cfd8661ac968699ed36
SstealzZ <antoine.veber@epitech.eu>
2023-07-09 00:39:30 +0200

    Modify Json.java - Add function to detect two same file

--- src/main/java/fr/sstealzz/data/Json.java
+++ src/main/java/fr/sstealzz/data/Json.java
@@ -25,29 +25,29 @@ public class Json {
     }
 
     public void append(Object data) {
-    Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    List<Object> dataList = read(); // lire les données existantes
-    dataList.add(data); // ajouter les nouvelles données à la fin de la liste
-
-    try (FileWriter writer = new FileWriter(file)) {
-        gson.toJson(dataList, writer); // écrire la liste mise à jour dans le fichier JSON
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-}   
-
-    public void init() {
-    if (!file.exists()) {
-        List<Object> dataList = new ArrayList<>();
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        List<Object> dataList = read(); // lire les données existantes
+        dataList.add(data); // ajouter les nouvelles données à la fin de la liste
 
         try (FileWriter writer = new FileWriter(file)) {
-            gson.toJson(dataList, writer);
+            gson.toJson(dataList, writer); // écrire la liste mise à jour dans le fichier JSON
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-}
+
+    public void init() {
+        if (!file.exists()) {
+            List<Object> dataList = new ArrayList<>();
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+            try (FileWriter writer = new FileWriter(file)) {
+                gson.toJson(dataList, writer);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     public List<Object> read() {
         Gson gson = new Gson();
@@ -114,5 +114,20 @@ public class Json {
     public boolean isExist(String name) {
         List<String> names = getNames("config.json");
         return names.contains(name);
+    }
+    
+    public boolean isAlreadyExist(Data data) throws IOException{
+        List<Data> dataList = getData();
+        for (Data existingData : dataList) {
+            if (existingData.getNameFile().equals(data.getNameFile())) {
+                if (existingData.getPathFile().equals(data.getPathFile())) {
+                    continue;
+                }
+                else {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
